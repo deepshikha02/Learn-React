57cd516269926329bafbf8f1ee3dd664e8d0793a
I303390 <deepshikha.mohanta@sap.com>
2018-06-07 16:30:09 +0530

    Forms
    Dynamically changing the state of DOM in react
    
    https://www.udemy.com/react-fundamentals/learn/v4/t/lecture/9167802?start=0

--- my-react-app/src/containers/series/index.js
+++ my-react-app/src/containers/series/index.js
@@ -7,9 +7,11 @@ class Series extends Component {
         data : []
       };
     
-      componentDidMount(){
-      
-        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+      componentDidMount(){}
+
+      onSeriesInputChange = e => {
+          console.log(e);
+          fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then(response => response.json())
         .then(json => {console.log(json)
         this.setState({
@@ -22,6 +24,9 @@ class Series extends Component {
         return (
             <div>
             <div> series container </div>
+            <div>
+                <input type="text" onChange={this.onSeriesInputChange} />
+            </div>
             The length of state data array is : {this.state.data.length}
             <SeriesList list={this.state.data}></SeriesList>
             </div>
