e0024ed9cd626839195abeb9b46e741287551606
I303390 <deepshikha.mohanta@sap.com>
2018-06-07 16:17:04 +0530

    Refactoring list component
    Adding a component within another component
    
    https://www.udemy.com/react-fundamentals/learn/v4/t/lecture/9156068?start=0

--- my-react-app/src/components/SeriesList/index.js
+++ my-react-app/src/components/SeriesList/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './style.css';
 
+const SeriesListItem = ({data}) => (
+    <li key={data.show.id}>{data.show.name}</li>
+)
+
 const SeriesList = (props) => {
     return (
         <div>
         <div>Series List Component</div>
         <ul className="series-list">
-            {props.list.map(data => (<li key={data.show.id}>{data.show.name}</li>))}
+            {
+                props.list.map(data => (
+                    <SeriesListItem key={data.show.id} data={data}></SeriesListItem>
+                    )
+                )
+            }
         </ul>
         </div>
     );
