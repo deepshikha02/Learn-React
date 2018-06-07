575c886ab8a6794fd3874a3adecb12dc502ce23d
I303390 <deepshikha.mohanta@sap.com>
2018-06-07 12:05:40 +0530

    Creating a basic hello world
    import React from 'react'; //react elements
    import ReactDOM from 'react-dom'; // used to render app. acts as an interaction
    import './index.css'; // css file
    import App from './App'; // app component that gets rendered.
    import registerServiceWorker from './registerServiceWorker'; //helps the app load faster on subsequent app load
    
    ReactDOM.render(<app component where you need to render>,<root element where it needs to be rendered>, <callback if needed>);
    
    React.createElement(<element>,<props {}>, <value_in_the_element>);
    
    https://www.udemy.com/react-fundamentals/learn/v4/t/lecture/8816014?start=0

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const greeting  = React.createElement('h1',{}, 'hello world');
+// React.createElement(<element>,<props {}>, <value_in_the_element>);
+
+ReactDOM.render(greeting, document.getElementById('root'));
 registerServiceWorker();
