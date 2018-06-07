d8a38b4c61883f049c39b62d2dd215f1e1260fd0
I303390 <deepshikha.mohanta@sap.com>
2018-06-07 17:31:18 +0530

    Conditional rendering
    https://www.udemy.com/react-fundamentals/learn/v4/t/lecture/9177778?start=0

--- my-react-app/src/containers/series/index.js
+++ my-react-app/src/containers/series/index.js
@@ -4,30 +4,53 @@ import SeriesList from '../../components/SeriesList';
 class Series extends Component {
 
     state = {
-        data : []
+        data : [],
+        seriesName : "",
+        isFetching : false
       };
     
       componentDidMount(){}
 
       onSeriesInputChange = e => {
           console.log(e);
+          this.setState({
+              seriesName : e.target.value,
+              isFetching : true
+          });
           fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
-        .then(response => response.json())
-        .then(json => {console.log(json)
-        this.setState({
-          data : json
-        });
-        });
+            .then(response => response.json())
+            .then(json => {console.log(json)
+            this.setState({
+            data : json,
+            isFetching : false
+            });
+            });
       }
 
     render(){
+        const { data, seriesName, isFetching} = this.state;
         return (
             <div>
             <div> series container </div>
+            {
+                data.length === 0 && seriesName.trim() === '' && 
+                <p>Please enter series name into the input</p>
+            }
+            {
+                data.length === 0 && seriesName.trim() !== '' && 
+                <p> Series not found </p>
+            }
+            {
+                isFetching && <p>Loading...</p>
+            }
+            {
+                !isFetching && <p>Loaded</p>
+            }
             <div>
-                <input type="text" onChange={this.onSeriesInputChange} />
+                <input type="text" onChange={this.onSeriesInputChange}  value={this.state.seriesName}/>
             </div>
             The length of state data array is : {this.state.data.length}
+            
             <SeriesList list={this.state.data}></SeriesList>
             </div>
         );
